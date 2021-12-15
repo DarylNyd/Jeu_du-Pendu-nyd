939c16c40ec28f50801dd0fc5996587272af6af1
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 19:43:07 +0100

    Update App.js

--- pendu/src/App.js
+++ pendu/src/App.js
@@ -1,52 +1,24 @@
 
 import './App.css';
-import { useState, useEffect } from 'react'
-import {CallApi} from './score'
 import Content from './components/content';
-import Splash from './theme/splash'
-import  { ThemeProvider } from "styled-components";
+import ThemeContextProvider from './theme/theme';
 
 
 const GOOD_LETTER = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnéàâêè";
-const LightTheme ={
-  pageBackground: "white",
-  fontColor:"black"
-};
-
-const DarkTheme ={
-  pageBackground: "#282c36",
-  fontColor:"white"
-}; 
- const themes = {
-   light:LightTheme,
-   dark:DarkTheme,
- }
-
 
 
 
 
 function App() {
-  const [theme, setTheme] = useState("light");
-
-  const themeToggler = () => {
-    theme === 'Light' ? setTheme("dark") : setTheme("Light");
-  };
+  
   return (
-    //<ThemeProvider theme={themes[theme]}>
-    //<Splash theme={theme} setTheme={setTheme}>
+    console.log("hello"),
     <div className="App">
-      <header className="App-header">
-       <h1>Animal Finder</h1>
-       
-       <Content />
-       
-       <p>Classement des Joueurs</p>
-       <CallApi></CallApi>
-      </header>
+      <ThemeContextProvider>
+          <Content /> 
+      </ThemeContextProvider>
     </div>
-   // </Splash>
-    //</ThemeProvider>
+   
   );
 }
 
