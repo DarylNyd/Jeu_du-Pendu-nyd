6443070d7d911a1293040d5168d899ffd6e3c6fb
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 19:52:37 +0100

    Update App.js

--- pendu/src/App.js
+++ pendu/src/App.js
@@ -14,9 +14,9 @@ function App() {
   return (
     console.log("hello"),
     <div className="App">
-      <ThemeContextProvider>
+      
           <Content /> 
-      </ThemeContextProvider>
+      
     </div>
    
   );
