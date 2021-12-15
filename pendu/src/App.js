dca58624eabec076623ca8aaa1295b6d597d1133
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 16:35:02 +0100

    Update App.js

--- pendu/src/App.js
+++ pendu/src/App.js
@@ -1,17 +1,47 @@
 
 import './App.css';
+import React, { useState } from 'react'
+import {CallApi} from './score'
 import Content from './components/content';
+import Splash from './theme/splash'
+import  { ThemeProvider } from "styled-components";
+
+const LightTheme ={
+  pageBackground: "white",
+  fontColor:"black"
+};
+
+const DarkTheme ={
+  pageBackground: "#282c36",
+  fontColor:"white"
+}; 
+ const themes = {
+   light:LightTheme,
+   dark:DarkTheme,
+ }
+
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === 'Light' ? setTheme("dark") : setTheme("Light");
+  };
   return (
+    //<ThemeProvider theme={themes[theme]}>
+    //<Splash theme={theme} setTheme={setTheme}>
     <div className="App">
       <header className="App-header">
        <h1>Animal Finder</h1>
+       
        <Content />
        
        <p>Classement des Joueurs</p>
+       <CallApi></CallApi>
       </header>
     </div>
+   // </Splash>
+    //</ThemeProvider>
   );
 }
 
