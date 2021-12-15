52df226153fd8cb7cb838d2ed937c06d5bb924a5
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 20:19:12 +0100

    Update Button.js

--- pendu/src/components/Button.js
+++ pendu/src/components/Button.js
@@ -1,8 +1,8 @@
 import React from "react"
 import "./button.css"
 
-export default function Button ({onclick: handleClick, value}) {
+export default function Button ({onclick: getData, value}) {
     return(
-    <button onClick={handleClick}>{value}</button>
+    <button onClick={getData}>{value}</button>
     )}
  
