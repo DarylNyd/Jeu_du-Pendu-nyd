c158621b250441fcece55aa744abe29039deae1e
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 23:00:59 +0100

    Update content.js

--- pendu/src/components/content.js
+++ pendu/src/components/content.js
@@ -2,22 +2,19 @@ import React from "react";
 import './content.css'
 import CallWord from "../wordapi";
 import {CallApi} from '../score';
-import react, {useContext} from 'react' ;
-import { ThemeContext } from '../../theme/theme';
 import Button1 from "./button1";
+
 
 
 
   export default function Content () {
     
-    const {theme} = useContext(ThemeContext);
+  
       
     
       return (
-        <div
-        className={theme ? 'contenu light' : 'contenu dark'}
-        >
-            <Button1 value={'DarkMode'} />
+          <div className="App-header" >
+              <Button1 value={'Change mode'} />
               <h1>Animal Finder</h1>
                
               <h2>Trouver d'animaux</h2>
