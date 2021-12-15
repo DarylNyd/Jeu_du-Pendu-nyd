ad0671d0e0dbad6767f88967ee80bba5e19ee1ba
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 19:43:29 +0100

    Update wordapi.js

--- pendu/src/wordapi.js
+++ pendu/src/wordapi.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import Button from "./components/Button";
 export function CallWord() {
+    const [motdev, setMotdev] = useState("");
     const [words, setData] = useState(undefined);
     useEffect(() => {
         (async () => {
@@ -14,12 +16,19 @@ export function CallWord() {
         return await dataJson.json();
     };
 
+    function handlelick(Mot){
+        setMotdev(Mot);
+
+    }
+
     if (!words) {
         return <p>Just Wait a minute</p>;
     }
     return (
         <div>
             <p>{words.data.word}</p>
+
+            <Button value={'Change word'} onclick={() => fetch()}/>
         </div>
     );
     }
