2842a114c83485b5a3ae226419fdccbbe088127e
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2021-12-15 17:14:08 +0100

    Update score.js

--- pendu/src/score.js
+++ pendu/src/score.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './score.css'
 export function CallApi () {
     const [ score, setdata] = useState(undefined);
     useEffect(() => {
@@ -18,8 +19,12 @@ export function CallApi () {
         return <p>waiting</p>;
     }
     return (
-        <div>
-            <p>{score.data.map(user => <div><p>{user.username}</p> <p>{user.score}</p> <p><img src={user.avatar} /></p></div>)}</p>
+        <div className="score1">
+            <p>{score.data.map(user => <div className="score"><table>
+                <th><td><img src={user.avatar} /></td>
+                    <td> {user.username}</td> 
+                    <td>{user.score}</td>
+                </th></table></div>)}</p>
             <p>{score.data[0].username}</p>
             <p>{score.data[0].score}</p>
             <p><img src={score.data[0].avatar} /></p>
